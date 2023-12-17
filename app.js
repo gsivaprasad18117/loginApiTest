bb1237640c532c6e9e750f1608845eded0f6a34d
gsivaprasad18117 <gsivaprasad18117@gmail.com>
2023-12-17 17:43:29 +0530

    git changes

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const app = express()
 
 //slash API
 app.get('/',async(request,response)=>{
-    response.send("It's working priya")
+    response.send("It's working bott")
 })
 
 app.listen(3000,()=>{
     console.log('ok')
 })
+
+module.exports= app
